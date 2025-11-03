c79b56cba7b2252d1b437ea9e784c6dabaf4eb8d
Dmytro Volovyk <dima.volovyk@gmail.com>
2025-11-03 22:46:09 +0200

    fix proto path

--- proto_rust/build.rs
+++ proto_rust/build.rs
@@ -3,8 +3,8 @@ use protobuf_codegen::Codegen;
 fn main() {
     Codegen::new()
         .protoc()
-        .includes(&["../stil_proto"])
-        .input("../stil_proto/hyprland.proto")
+        .includes(&["../proto"])
+        .input("../proto/hyprland.proto")
         .out_dir("src/proto")
         .run_from_script();
 }
