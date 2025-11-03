83082ec14e954099850f2f3ab021d17881cff50c
Dmytro Volovyk <dima.volovyk@gmail.com>
2025-11-03 23:00:02 +0200

    user serde to parse hyprland ctl response

--- daemon/src/hyprland/ctl/clients.rs
+++ daemon/src/hyprland/ctl/clients.rs
@@ -1,81 +1,28 @@
-use crate::hyprland::HyprCtlCmd;
+use super::HyprCtlCmd;
+use serde::Deserialize;
 
-// TODO: parse as json `hyprctl -j clients`
-#[derive(Debug, Default)]
+#[derive(Debug, Deserialize)]
+pub struct Workspace {
+    pub id: u32,
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Client {
-    pub window_address: String,
+    pub address: String,
     pub class: String,
-    pub workspace_id: String,
+    pub workspace: Workspace,
 }
 
-impl Client {
-    #[inline]
-    fn with_class(mut self, class: &str) -> Self {
-        self.class.push_str(class);
-        self
-    }
-
-    #[inline]
-    fn with_workspace_id(mut self, workspace: &str) -> Self {
-        self.workspace_id.push_str(workspace);
-        self
-    }
-
-    #[inline]
-    fn with_window_address(mut self, window_address: &str) -> Self {
-        self.window_address.push_str(window_address);
-        self
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct Clients(pub Vec<Client>);
 
 impl TryFrom<&str> for Clients {
     type Error = ();
 
+    #[inline]
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let mut clients = Vec::new();
-        let mut current = None;
-
-        for line in value.lines() {
-            let trimmed = line.trim();
-
-            if trimmed.is_empty() {
-                if let Some(client) = current.take() {
-                    clients.push(client);
-                }
-                continue;
-            }
-
-            if trimmed.starts_with("Window") {
-                let mut parts = trimmed.splitn(3, ' ');
-                
-                // skip "Window"
-                if let None = parts.next() {
-                    continue;
-                }
-                
-                if let Some(address) = parts.next() {
-                    let mut client = Client::default();
-                    client.window_address = address.to_string();
-                    current = Some(client);
-                }
-            } else {
-                let mut parts = trimmed.splitn(2, ':');
-                let Some(key) = parts.next() else { continue };
-                let Some(value) = parts.next().map(|v| v.trim()) else { continue };
-
-                current = match key {
-                    "class" => current.map(|client| client.with_class(value)),
-                    "workspace" => current.map(|client| client.with_workspace_id(value)),
-                    "window_address" => current.map(|client| client.with_window_address(value)),
-                    _ => continue,
-                };
-            }
-        }
-
-        Ok(Clients(clients))
+        serde_json::from_str(value).map_err(|_| ()).map(|vec| Self(vec))
     }
 }
 
@@ -85,7 +32,7 @@ pub struct ClientsCmd;
 impl ToString for ClientsCmd {
     #[inline]
     fn to_string(&self) -> String {
-        "clients".to_string()
+        "-j/clients".to_string()
     }
 }
 
