1ac7e4ad187f28709a91f5bc137753c8f78d03a5
Dmytro Volovyk <dima.volovyk@gmail.com>
2025-11-05 15:49:09 +0200

    include

--- proto_rust/build.rs
+++ proto_rust/build.rs
@@ -4,6 +4,7 @@ fn main() {
     let result = CodeGen::new()
         .include("../proto")
         .input("../proto/hyprland.proto")
+        .input("../proto/desktop.proto")
         .output_dir("src")
         .generate_and_compile();
 
