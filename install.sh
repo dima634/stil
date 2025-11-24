fd32c15336967dbad18937505de89c2733dcd5af
Dmytro Volovyk <dima.volovyk@gmail.com>
2025-11-24 20:34:51 +0200

    add build type arg

--- install.sh
+++ install.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-cargo build
-cmake --preset debug
-cmake --build --preset debug;
-sudo cmake --build --preset debug --target install
+BUILD_TYPE="${1:-debug}"
+
+if [ "$BUILD_TYPE" = "release" ]; then
+    cargo build --release
+else
+    cargo build
+fi
+cmake --preset ${BUILD_TYPE}
+cmake --build --preset ${BUILD_TYPE};
+sudo cmake --build --preset ${BUILD_TYPE} --target install
 
 pkill quickshell || true
 hyprctl dispatch exec quickshell
