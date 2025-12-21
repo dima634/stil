16273822ce5eee4525b827cef65f25daadf47f07
Dmytro Volovyk <dima.volovyk@gmail.com>
2025-12-21 17:33:52 +0200

    add workspace mod

--- taskbar/src/ui/mod.rs
+++ taskbar/src/ui/mod.rs
@@ -1,11 +1,9 @@
 mod taskbar;
 mod taskbar_item;
 mod window;
-mod workspace_list;
-mod workspace_model;
+mod workspace;
 
 pub use taskbar::Taskbar;
 pub use taskbar_item::{StatusBar, TaskbarItem};
 pub use window::{WindowList, WindowModel};
-pub use workspace_list::WorkspaceList;
-pub use workspace_model::WorkspaceModel;
+pub use workspace::{WorkspaceList, WorkspaceModel};
---
+++ taskbar/src/ui/workspace/mod.rs
@@ -0,0 +1,5 @@
+mod workspace_list;
+mod workspace_model;
+
+pub use workspace_list::WorkspaceList;
+pub use workspace_model::WorkspaceModel;
